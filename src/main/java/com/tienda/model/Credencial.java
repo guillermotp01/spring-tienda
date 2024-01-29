a3bd02df7f88b8f522158515652bd660a9948d44
Franklin Guevara <franklingueflo@hotmail.com>
2024-01-29 18:50:44 -0500

    agregando Credencial y modificando usuario

---
+++ src/main/java/com/tienda/model/Credencial.java
@@ -0,0 +1,37 @@
+package com.tienda.model;
+
+
+import com.tienda.records.CrearCredencial;
+import com.tienda.utils.PasswordHash256;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_credenciales")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idCredencial")
+@ToString
+public class Credencial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCredencial;
+    @Column(length = 255, nullable = false, unique = true)
+    private String email;
+    @Column(length = 255, nullable = false)
+    private String password;
+    @Column(length = 16, nullable = false, unique = true)
+    private String salt;
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idUsuario")
+    private Usuario usuario;
+
+    public Credencial(CrearCredencial datos) {
+        this.salt = PasswordHash256.gereratedSalt();
+        this.email = datos.email();
+        this.password = PasswordHash256.generatedHash(salt,datos.password());
+        this.usuario = new Usuario(datos.usuario());
+    }
+}
--- src/main/java/com/tienda/model/Usuario.java
+++ src/main/java/com/tienda/model/Usuario.java
@@ -1,83 +1,29 @@
 package com.tienda.model;
 
-public class Usuario {
-	private Integer id;
-	private String nombre;
-	private String username;
-	private String email;
-	private String direccion;
-	private String telefono;
-	private String tipo;
-	private String password;
-	
-	public Usuario() {
-	}
-	
-	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
-			String tipo, String password) {
-		this.id = id;
-		this.nombre = nombre;
-		this.username = username;
-		this.email = email;
-		this.direccion = direccion;
-		this.telefono = telefono;
-		this.tipo = tipo;
-		this.password = password;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", email=" + email
-				+ ", direccion=" + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password
-				+ "]";
-	}
+import com.tienda.records.CrearUsuario;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idUsuario")
+@ToString
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    @Column(length = 100,nullable = false)
+    private String nombre;
+    @Column(length = 9,nullable = false,unique = true)
+    private String telefono;
+
+    public Usuario(CrearUsuario datos) {
+        this.nombre = datos.nombre();
+        this.telefono = datos.telefono();
+    }
 }

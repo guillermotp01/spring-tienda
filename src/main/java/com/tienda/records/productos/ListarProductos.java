35dac236a33ec8fb53ef447c50905f5623e2e94e
Franklin Guevara <franklingueflo@hotmail.com>
2024-02-04 04:02:49 -0500

    agregar records para listar los productos

--- src/main/java/com/tienda/records/productos/ListarProductos.java
+++ src/main/java/com/tienda/records/productos/ListarProductos.java
@@ -4,17 +4,25 @@ import com.tienda.model.Producto;
 import com.tienda.model.Talla;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record ListarProductos(
         String nombre,
         String descripcion,
-        Map<String,Talla> tallas
+        Map<String,ListarTalla> tallas
 ) {
     public ListarProductos(Producto producto) {
         this(
                 producto.getNombre(),
                 producto.getDescripcion(),
                 producto.getTallas()
+                        .entrySet()
+                        .stream()
+                        .collect(Collectors
+                                .toMap(Map
+                                        .Entry::getKey,entry
+                                        -> new ListarTalla(entry
+                                        .getValue())))
         );
     }
 
--- src/main/java/com/tienda/records/productos/ListarTalla.java
+++ src/main/java/com/tienda/records/productos/ListarTalla.java
@@ -5,16 +5,23 @@ import com.tienda.model.Producto;
 import com.tienda.model.Talla;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record ListarTalla(
         String talla,
         Producto producto,
-        Map<String, ExistenciaProducto> existenciasDisponibles
+        Map<String, ListarExistenciaProducto> existenciasDisponibles
 ) {
     public ListarTalla(Talla datos){
         this(   datos.getTalla(),
                 datos.getProducto(),
-                datos.getExistenciaProductoMap()
+                datos.getExistenciaProductoMap().entrySet()
+                        .stream()
+                        .collect(Collectors
+                                .toMap(Map
+                                        .Entry::getKey,entry
+                                        -> new ListarExistenciaProducto(entry
+                                        .getValue())))
         );
     }
 

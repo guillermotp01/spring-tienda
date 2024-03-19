d448e3fb46380e46c82384fe2a706c40344f9da2
guillermotp01 <n00247337@gmail.com>
2024-03-19 16:28:00 -0500

    se realiza la eliminacion de archivos que no van a ser utilizados

--- src/main/java/com/tienda/model/dao/UsuarioDao.java
+++
@@ -1,8 +0,0 @@
-package com.tienda.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tienda.model.entity.Usuario;
-
-public interface UsuarioDao extends JpaRepository<Usuario, Integer>{
-
-}
